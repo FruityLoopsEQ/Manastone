cb4988b69a243dad8a7dd6f1dabe19d1764ad623
Shaun Dern <shaun@substantial.com>
2024-01-07 21:41:33 -0700

    Logger library is imported

--- init.lua
+++ init.lua
@@ -1,37 +1,18 @@
 local mq = require("mq")
-
-local Logger = {}
-
-Logger.loglevels = {
-    ["debug"] = { level = 1, abbreviation = "DEBUG", },
-    ["info"]  = { level = 2, abbreviation = "INFO" }
-}
-
-function Logger.Output(paramLogLevel, message)
-    if Logger.loglevels[Logger.loglevel:lower()].level <= Logger.loglevels[paramLogLevel].level then
-        print(string.format("%s[%s] :: %s", Logger.prefix, Logger.loglevels[paramLogLevel].abbreviation, message))
-    end
-end
-
-function Logger.Debug(message)
-    Logger.Output("debug", message)
-end
-
-function Logger.Info(message)
-    Logger.Output("info", message)
-end
+local Logger = require("PortBot.lib.Logger")
 
 Logger.prefix = "Manastone"
 Logger.loglevel = "info"
 
 STOP_MANA_PCT = 98
 STOP_HP_PCT = 40
+VERSION="1.0.0"
 
 local function manastone()
     while true do
         local manaPct = mq.TLO.Me.PctMana()
         if (manaPct >= STOP_MANA_PCT) then
-            Logger.Debug(string.format("Full Mana - %d", manaPct) .. "%")
+            Logger.Debug("Full Mana - %d", manaPct)
             return
         end
 
@@ -42,7 +23,7 @@ local function manastone()
         local casting = mq.TLO.Me.Casting()
 
         if (hpPct <= STOP_HP_PCT) then
-            Logger.Info(string.format("Low HP - %d", hpPct) .. "%")
+            Logger.Info("Low HP - %d", hpPct)
             return
         end
 
@@ -72,10 +53,10 @@ end
 local function inGame() return mq.TLO.MacroQuest.GameState() == "INGAME" end
 
 local function start()
-    Logger.Info("Start")
+    Logger.Info("Starting Version: %s", VERSION)
 
-    Logger.Info(string.format("Stop HitPoints Percentage=%s", STOP_HP_PCT))
-    Logger.Info(string.format("Stop Mana Percentage=%s", STOP_MANA_PCT))
+    Logger.Info("Stop HitPoints Percentage=%d", STOP_HP_PCT)
+    Logger.Info("Stop Mana Percentage=%d", STOP_MANA_PCT)
 
     while true do
         if inGame() then
